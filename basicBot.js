0ad3d0bccb7f325a749cadc09abfd6c329a869f4
WizzardDestroyer <wizzarddestroyer@gmail.com>
2015-07-16 17:09:41 -0500

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -252,9 +252,9 @@
             language: "english",
             chatLink: "https://rawgit.com/WizzardDestroyer/basicBot/master/lang/en.json",
             roomLock: false, // Requires an extension to re-load the script
-            startupCap: 1, // 1-200
-            startupVolume: 0, // 0-100
-            startupEmoji: false, // true or false
+            startupCap: 200, // 1-200
+            startupVolume: 15, // 0-100
+            startupEmoji: true, // true or false
             autowoot: true,
             autoskip: false,
             smartSkip: true,
